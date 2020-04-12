d6c87c70ce9d10ded941b1d02664404a43b1e323
M.K. Safi <msafi@msafi.com>
2020-04-12 14:07:27 -0700

    Make `createExecutable.ts` runnable locally

--- .github/action/createExecutable.ts
+++ .github/action/createExecutable.ts
@@ -1,3 +1,7 @@
+/**
+ * This file must be runnable on both the local development environment and
+ * on GitHub Actions
+ */
 /* eslint-disable no-console */
 import * as core from "@actions/core";
 import * as artifact from "@actions/artifact";
@@ -6,9 +10,11 @@ import * as packager from "electron-packager";
 import * as archiver from "archiver";
 import { basename } from "path";
 import * as electronInstaller from "electron-winstaller";
+import bluebird from "bluebird";
 import { getAppVersion } from "./getAppVersion";
 import { execWithErrorMessage } from "./utils";
 import pkgJson from "../../package.json";
+import { isOnGithubActions } from "./isOnGithubActions";
 
 const artifactClient = artifact.create();
 const appVersion = getAppVersion();
@@ -17,80 +23,82 @@ const electronPackagerArtifactDir = "artifacts/electronPackager";
 const { productName } = pkgJson;
 
 export const platformDefinitions = {
-  "macos-latest": {
-    electronPackagerPlatform: "darwin",
+  darwin: {
     archiveName: `${productName}-v${appVersion}-macOS.zip`,
   },
-  "ubuntu-latest": {
-    electronPackagerPlatform: "linux",
+  linux: {
     archiveName: `${productName}-v${appVersion}-Linux.zip`,
   },
-  "windows-latest": {
-    electronPackagerPlatform: "win32",
+  win32: {
     archiveName: `${productName}-v${appVersion}-Windows.exe`,
   },
 };
 
 export const createExecutable = async () => {
-  console.log(process.env);
-  // console.log("Building source code...");
-  // await execWithErrorMessage("npm run build", "`npm run build` failed");
+  console.log("Building source code...");
+  await execWithErrorMessage("npm run build", "`npm run build` failed");
 
-  // const os = core.getInput("os", { required: true });
-  // const { archiveName, electronPackagerPlatform } = platformDefinitions[os];
+  const os = process.platform;
+  const { archiveName } = platformDefinitions[os];
 
-  // console.log(`Creating Electron package on ${os}...`);
-  // await packager({
-  //   arch: "x64",
-  //   dir: "artifacts/webpack",
-  //   out: electronPackagerArtifactDir,
-  //   icon: "src/assets/orange",
-  //   overwrite: true,
-  //   platform: electronPackagerPlatform,
-  //   prune: false,
-  //   appVersion: getAppVersion(),
-  // });
+  console.log(`Creating Electron package on ${os}...`);
+  await packager({
+    arch: "x64",
+    dir: "artifacts/webpack",
+    out: electronPackagerArtifactDir,
+    icon: "src/assets/orange",
+    overwrite: true,
+    platform: os,
+    prune: false,
+    appVersion: getAppVersion(),
+  });
 
-  // if (os === "macos-latest") {
-  //   console.log("Compressing Electron package...");
-  //   await new Promise(resolve_ => {
-  //     const archive = archiver("zip", { zlib: { level: 9 } });
-  //     const output = fs.createWriteStream(
-  //       `${electronPackagerArtifactDir}/${archiveName}`,
-  //     );
-  //     archive.pipe(output);
-  //     archive.directory(
-  //       `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64/`,
-  //       false,
-  //     );
-  //     output.on("close", resolve_);
-  //     archive.on("error", error => {
-  //       console.error(error);
-  //       core.setFailed(`Could not create a zip archive on ${os}`);
-  //     });
-  //     archive.finalize();
-  //   });
-  // } else if (os === "windows-latest") {
-  //   console.log(`Creating ${archiveName}...`);
-  //   await electronInstaller.createWindowsInstaller({
-  //     appDirectory: `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64`,
-  //     outputDirectory: electronPackagerArtifactDir,
-  //     authors: "https://github.com/orange-org",
-  //     exe: `${productName}.exe`,
-  //     setupExe: archiveName,
-  //     version: appVersion,
-  //     name: productName,
-  //     title: productName,
-  //     iconUrl:
-  //       "https://raw.githubusercontent.com/msafi/orange/55d3cebd112e8e5ea0cf3a3ecf2bb6aa824ba3c4/src/assets/orange.ico",
-  //     setupIcon: "src/assets/orange.ico",
-  //   });
-  // }
+  if (os === "darwin") {
+    console.log("Compressing Electron package...");
+    await new Promise(resolve_ => {
+      const archive = archiver("zip", { zlib: { level: 9 } });
+      const output = fs.createWriteStream(
+        `${electronPackagerArtifactDir}/${archiveName}`,
+      );
+      archive.pipe(output);
+      archive.directory(
+        `${electronPackagerArtifactDir}/${productName}-${os}-x64/`,
+        false,
+      );
+      output.on("close", resolve_);
+      archive.on("error", error => {
+        console.error(error);
+        core.setFailed(`Could not create a zip archive on ${os}`);
+      });
+      archive.finalize();
+    });
+  } else if (os === "win32") {
+    console.log(`Creating ${archiveName}...`);
+    await electronInstaller.createWindowsInstaller({
+      appDirectory: `${electronPackagerArtifactDir}/${productName}-${os}-x64`,
+      outputDirectory: electronPackagerArtifactDir,
+      authors: "https://github.com/orange-org",
+      exe: `${productName}.exe`,
+      setupExe: archiveName,
+      version: appVersion,
+      name: productName,
+      title: productName,
+      iconUrl:
+        "https://raw.githubusercontent.com/msafi/orange/55d3cebd112e8e5ea0cf3a3ecf2bb6aa824ba3c4/src/assets/orange.ico",
+      setupIcon: "src/assets/orange.ico",
+    });
+  }
 
-  // console.log(`Uploading ${archiveName}...`);
-  // await artifactClient.uploadArtifact(
-  //   basename(archiveName),
-  //   [`${electronPackagerArtifactDir}/${archiveName}`],
-  //   electronPackagerArtifactDir,
-  // );
+  if (isOnGithubActions) {
+    console.log(`Uploading ${archiveName}...`);
+    await artifactClient.uploadArtifact(
+      basename(archiveName),
+      [`${electronPackagerArtifactDir}/${archiveName}`],
+      electronPackagerArtifactDir,
+    );
+  }
 };
+
+if (process.env.FILENAME === "createExecutable") {
+  bluebird.try(createExecutable).catch(console.error);
+}
