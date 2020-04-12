a44f8bd8bf27b83d4f38735b5dea4419bf99e383
M.K. Safi <msafi@msafi.com>
2020-04-12 13:24:27 -0700

    no message

--- .github/action/createExecutable.ts
+++ .github/action/createExecutable.ts
@@ -32,64 +32,65 @@ export const platformDefinitions = {
 };
 
 export const createExecutable = async () => {
-  console.log("Building source code...");
-  await execWithErrorMessage("npm run build", "`npm run build` failed");
+  console.log(process.env);
+  // console.log("Building source code...");
+  // await execWithErrorMessage("npm run build", "`npm run build` failed");
 
-  const os = core.getInput("os", { required: true });
-  const { archiveName, electronPackagerPlatform } = platformDefinitions[os];
+  // const os = core.getInput("os", { required: true });
+  // const { archiveName, electronPackagerPlatform } = platformDefinitions[os];
 
-  console.log(`Creating Electron package on ${os}...`);
-  await packager({
-    arch: "x64",
-    dir: "artifacts/webpack",
-    out: electronPackagerArtifactDir,
-    icon: "src/assets/orange",
-    overwrite: true,
-    platform: electronPackagerPlatform,
-    prune: false,
-    appVersion: getAppVersion(),
-  });
+  // console.log(`Creating Electron package on ${os}...`);
+  // await packager({
+  //   arch: "x64",
+  //   dir: "artifacts/webpack",
+  //   out: electronPackagerArtifactDir,
+  //   icon: "src/assets/orange",
+  //   overwrite: true,
+  //   platform: electronPackagerPlatform,
+  //   prune: false,
+  //   appVersion: getAppVersion(),
+  // });
 
-  if (os === "macos-latest") {
-    console.log("Compressing Electron package...");
-    await new Promise(resolve_ => {
-      const archive = archiver("zip", { zlib: { level: 9 } });
-      const output = fs.createWriteStream(
-        `${electronPackagerArtifactDir}/${archiveName}`,
-      );
-      archive.pipe(output);
-      archive.directory(
-        `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64/`,
-        false,
-      );
-      output.on("close", resolve_);
-      archive.on("error", error => {
-        console.error(error);
-        core.setFailed(`Could not create a zip archive on ${os}`);
-      });
-      archive.finalize();
-    });
-  } else if (os === "windows-latest") {
-    console.log(`Creating ${archiveName}...`);
-    await electronInstaller.createWindowsInstaller({
-      appDirectory: `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64`,
-      outputDirectory: electronPackagerArtifactDir,
-      authors: "https://github.com/orange-org",
-      exe: `${productName}.exe`,
-      setupExe: archiveName,
-      version: appVersion,
-      name: productName,
-      title: productName,
-      iconUrl:
-        "https://raw.githubusercontent.com/msafi/orange/55d3cebd112e8e5ea0cf3a3ecf2bb6aa824ba3c4/src/assets/orange.ico",
-      setupIcon: "src/assets/orange.ico",
-    });
-  }
+  // if (os === "macos-latest") {
+  //   console.log("Compressing Electron package...");
+  //   await new Promise(resolve_ => {
+  //     const archive = archiver("zip", { zlib: { level: 9 } });
+  //     const output = fs.createWriteStream(
+  //       `${electronPackagerArtifactDir}/${archiveName}`,
+  //     );
+  //     archive.pipe(output);
+  //     archive.directory(
+  //       `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64/`,
+  //       false,
+  //     );
+  //     output.on("close", resolve_);
+  //     archive.on("error", error => {
+  //       console.error(error);
+  //       core.setFailed(`Could not create a zip archive on ${os}`);
+  //     });
+  //     archive.finalize();
+  //   });
+  // } else if (os === "windows-latest") {
+  //   console.log(`Creating ${archiveName}...`);
+  //   await electronInstaller.createWindowsInstaller({
+  //     appDirectory: `${electronPackagerArtifactDir}/${productName}-${electronPackagerPlatform}-x64`,
+  //     outputDirectory: electronPackagerArtifactDir,
+  //     authors: "https://github.com/orange-org",
+  //     exe: `${productName}.exe`,
+  //     setupExe: archiveName,
+  //     version: appVersion,
+  //     name: productName,
+  //     title: productName,
+  //     iconUrl:
+  //       "https://raw.githubusercontent.com/msafi/orange/55d3cebd112e8e5ea0cf3a3ecf2bb6aa824ba3c4/src/assets/orange.ico",
+  //     setupIcon: "src/assets/orange.ico",
+  //   });
+  // }
 
-  console.log(`Uploading ${archiveName}...`);
-  await artifactClient.uploadArtifact(
-    basename(archiveName),
-    [`${electronPackagerArtifactDir}/${archiveName}`],
-    electronPackagerArtifactDir,
-  );
+  // console.log(`Uploading ${archiveName}...`);
+  // await artifactClient.uploadArtifact(
+  //   basename(archiveName),
+  //   [`${electronPackagerArtifactDir}/${archiveName}`],
+  //   electronPackagerArtifactDir,
+  // );
 };
